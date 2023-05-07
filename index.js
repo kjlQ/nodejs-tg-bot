7cdc9b1503e4624ce4968e3e0d2187c6d2a3a6bf
Roma <roma@MacBook-Air-Roma.local>
2023-05-07 18:14:46 +0300

    implemented menu , info and game buttons

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const TelegramApi = require("node-telegram-bot-api");
+require("dotenv").config();
 
-const token = "6113366027:AAFA_ROgDaqbRgXerIps3CJGDleGessN8Pg";
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramApi(token, { polling: true });
 
 const chats = {};
 
-const gameOption = {
+const option = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
       [
         { text: "1", callback_data: 1 },
         { text: "2", callback_data: 2 },
+        { text: "3", callback_data: 3 },
       ],
     ],
   }),
+};
+
+const retryOption = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [[{ text: "Yes", callback_data: "accepted" }]],
+  }),
+};
+
+const startRound = (chatId) => {
+  const randInt = Math.ceil(Math.random() * 3);
+  chats[chatId] = randInt;
+  return bot.sendMessage(chatId, "Take guess which number i've generated", option);
 };
 
 bot.setMyCommands([
@@ -33,23 +47,36 @@ bot.setMyCommands([
     const { first_name, username } = msg.from;
     const chatId = msg.chat.id;
     const text = msg.text;
-    if (text === "/start") {
-      return bot.sendMessage(chatId, `Hey, how can i help you ?`);
+    switch (text) {
+      case "/start":
+        await bot.sendSticker(
+          chatId,
+          "https://tlgrm.eu/_/stickers/039/535/0395358a-70e2-437f-9459-4101b904ede5/4.webp"
+        );
+
+        return bot.sendMessage(chatId, `Hi , the bot is started`);
+      case "/info":
+        return bot.sendMessage(chatId, `Your name is ${first_name} , your username is @${username}`);
+      case "/game":
+        return startRound(chatId);
+      default:
+        return bot.sendMessage(chatId, "Command not found");
     }
-    if (text === "/info") {
-      return bot.sendMessage(chatId, `Your name is ${first_name} , your username is ${username}`);
-    }
-    if (text === "/game") {
-      const randInt = Math.ceil(Math.random() * 2);
-      chats[chatId] = randInt;
-      return bot.sendMessage(chatId, "Guess the number" + randInt, gameOption);
-    }
-    return bot.sendMessage(chatId, "Command not found");
   });
   bot.on("callback_query", async (msg) => {
-    console.log(msg);
     const chatId = msg.message.chat.id;
     const { data } = msg;
-    return bot.sendMessage(chatId, `You typed on ${data} , ${chats[chatId]}`);
+    switch (data) {
+      case String(chats[chatId]):
+        return bot.sendMessage(chatId, `Exactly, the randomly generated number was ${data}`);
+      case "accepted":
+        return startRound(chatId);
+      default:
+        return bot.sendMessage(
+          chatId,
+          `You choose ${data}, the answer is ${chats[chatId]} .Want to play again ?`,
+          retryOption
+        );
+    }
   });
 })();
